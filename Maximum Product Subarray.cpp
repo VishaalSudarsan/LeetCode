287ac7b18749862c046c1da5d8ad268ab37b7a44
Vishaal Sudarsan <73996658+VishaalSudarsan@users.noreply.github.com>
2021-05-28 19:12:46 +0530

    Update Maximum Product Subarray.cpp

--- Maximum Product Subarray.cpp
+++ Maximum Product Subarray.cpp
@@ -1,16 +1,66 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        int size = nums.size(), max_prod = INT_MIN, temp_prod = 1;
-        for(int i=0;i<size;i++)
+    
+    int maxProductInner(vector<int>& nums)
+    {
+        if(nums.size()==1) return nums[0];
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        int negatives = count_if(nums.begin(), nums.end(), [](int x){ return x < 0;});
+        int result;
+        if(negatives%2 == 0)
+            result = accumulate(nums.begin(), nums.end(), 1, std::multiplies<>());
+        else
         {
-            temp_prod = 1;
-            for(int j=i;j<size;j++)            {
-                temp_prod *= nums[j];
-                if(temp_prod > max_prod)
-                    max_prod = temp_prod;
+            int product_till_first_negative=1, product_after_last_negative=1;
+            int first_negative_pos, last_negative_pos;
+            int i=0;
+            while(i < nums.size()-1 && nums[i]>0)
+            {
+                product_till_first_negative *= nums[i];
+                i++;
+            }
+            first_negative_pos = i;
+            i=nums.size()-1;
+            while(i>=0 && nums[i]>0)
+            {
+                product_after_last_negative *= nums[i];
+                i--;
+            }
+            last_negative_pos = i;
+            cout<<product_till_first_negative<<" "<<product_after_last_negative<<endl;
+            cout<<first_negative_pos<<" "<<last_negative_pos<<endl;
+            if(product_till_first_negative*abs(nums[first_negative_pos]) > product_after_last_negative*abs(nums[last_negative_pos]))
+                result = accumulate(nums.begin(), nums.begin()+last_negative_pos, 1, std::multiplies<int>());
+            else
+                result = accumulate(nums.begin()+first_negative_pos+1, nums.end(), 1, std::multiplies<int>());
+        }
+        return result;        
+    }
+    
+    int maxProduct(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        int result;
+        vector<int> zeros, inner_prods;
+        nums.insert(nums.begin(),0);
+        nums.push_back(0);
+        for(int i=0;i<nums.size();i++)
+            if(nums[i] == 0)
+                zeros.push_back(i);
+        
+        if(zeros.size() - nums.size() == 0) return 0;
+
+        for(int i=0;i<zeros.size()-1;i++)
+        {
+            if(zeros[i]+1 != zeros[i+1])
+            {
+                vector<int> inner_nums(nums.begin()+zeros[i]+1, nums.begin()+zeros[i+1]);
+                inner_prods.push_back(maxProductInner(inner_nums));
             }
         }
-        return max_prod;
+
+        result = *max_element(inner_prods.begin(), inner_prods.end());
+        if(zeros.size()>2 && result < 0) result = 0;
+        return result;
     }
 };
