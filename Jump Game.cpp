2514068463a07775ea465a5e65c5bca999be1872
Vishaal Sudarsan <73996658+VishaalSudarsan@users.noreply.github.com>
2021-07-13 01:55:15 +0530

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
 bool canJump(vector<int>& nums) {
+    //The idea is to check from the last element
+    //Check if you can reach from the second last element to
+    //the last element ? If yes check if the second last element
+    //can be reached from third last element. If No then check if
+    //the last element can be reached from the third last element
+    //Do this iteratively until you've reached the first index.
+    
     int size = nums.size();
     int last_true_pos = size - 1;
     for (int i = size - 2; i > 0; i--)
