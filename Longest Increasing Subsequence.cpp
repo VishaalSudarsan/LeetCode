4eed1de3a749e2f2439324611decba4835bf37ad
Vishaal Sudarsan <73996658+VishaalSudarsan@users.noreply.github.com>
2021-07-13 01:25:03 +0530

    O(nlogn) solution

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int tempmax = 1, maxlength = 1;
+        vector<int>::iterator index;
         vector<int> dp;
-        dp.push_back(1);
+        dp.push_back(nums[0]);
         for(int i=1;i<nums.size();i++)
         {
-            tempmax = 1;
-            for(int j=i-1;j>=0;j--)           
-                if(nums[i] > nums[j] && tempmax < dp[j] + 1)
-                    tempmax = dp[j] + 1;
-            dp.push_back(tempmax);
-            if(maxlength < tempmax) maxlength = tempmax;
+            index = lower_bound(dp.begin(), dp.end(), nums[i]);
+            if(index == dp.end())
+                dp.push_back(nums[i]);
+            else if(nums[i] < *index)
+                *index = nums[i];
         }
-        return maxlength;
+        return dp.size();
     }
 };
