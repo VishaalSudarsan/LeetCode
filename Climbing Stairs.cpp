416fa3bfa70a04b73733576f7bc7f79ad26e0059
Vishaal Sudarsan <73996658+VishaalSudarsan@users.noreply.github.com>
2021-09-13 15:52:43 +0530

    Update Climbing Stairs.cpp

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -1,3 +1,4 @@
+//Method1
 class Solution {
 public:
     int climbStairsInner(int n, vector<int>& dp)
@@ -18,3 +19,25 @@ public:
         return climbStairsInner(n, dp);
     }
 };
+
+
+//Method2
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+        vector<int> dp(n+2, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        if(n<=2)
+            return dp[n];
+        else
+        {
+            for(int i=3;i<n+1;i++)
+                dp[i] = dp[i-1] + dp[i-2];
+            return dp[n];
+        }
+    }
+};
+
