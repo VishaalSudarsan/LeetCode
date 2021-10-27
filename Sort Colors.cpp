053fde9351a839ecf2df46002d9141c231908c3f
Vishaal Sudarsan <73996658+VishaalSudarsan@users.noreply.github.com>
2021-10-27 21:13:19 +0530

    Update Sort Colors.cpp

--- Sort Colors.cpp
+++ Sort Colors.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
 public:
     void sortColors(vector<int>& nums) 
@@ -6,5 +7,25 @@ public:
             for(int j=i+1;j<nums.size();j++)
                 if(nums[i]>nums[j])
                     swap(nums[i],nums[j]);
+    }
+};
+
+//Method2
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        array<int, 3> colors = {0,1,2};
+        unordered_map<int, int> color_map;
+        for(int color : colors)
+            color_map[color] = 0;
+        
+        for(int elem : nums)
+            color_map[elem]++;
+        
+        int pos = 0;
+        for(int color : colors)
+            while(color_map[color]--)
+                nums[pos++] = color;
     }
 };
